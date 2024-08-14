dec330c86456d1b82f1236944c1ab1af66102cb9
cakefeelsgood <bloom_vk@naver.com>
2024-08-15 02:53:35 +0900

    first commit

---
+++ src/test/java/com/cake/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cake.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
